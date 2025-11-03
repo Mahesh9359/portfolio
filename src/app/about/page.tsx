c748645ec07fe2aafb91451f443d27b970eb29a3
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-11-04 00:05:34 +0530

    About page created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import About from '@/components/About'
+
+const AboutPage: React.FC = () => {
+  return (
+    <div className="w-full">
+      <About />
+    </div>
+  );
+};
+
+export default AboutPage;
